b7775a9cc39f0d5949f1b9e3ca3ae30b6a78786e
amirRezaei <amir.reazei1999@yahoo.com>
2019-06-14 11:39:40 +0430

    Add very first Login Page

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,18 +1,113 @@
 import React, { Component } from 'react';
-// import Aux from "../../hoc/AUX";
-import  './Login.css'
+import  './Login.css';
+import "../../bootstrap/css/bootstrap.min.css";
+import {Button} from "react-bootstrap";
+import {FormGroup} from "react-bootstrap";
+import logo from "../../assets/charity2-01.png";
 class Login extends Component {
+  state = {
+    data : {
+      UserName : "",
+      Password : "",
+    }
+  }
+
+  handle_login = (e, data) => {
+    // e.preventDefault();
+    console.log("ready to send data")
+    fetch('http://localhost:8000/token-auth/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.user.username
+        });
+      });
+  };
   
-
-
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState(prevState => {
+      const newState = {...prevState};
+      newState.data[name] = value;
+      return newState;
+    })
+    console.log(name,value);
+    console.log(this.state.data);
+  }
+  componentDidMount() {
+    document.body.classList.add("background");
+  } 
   
   render() {
+    
+    let LoginForm = (
+      <div className="container contain_1">
+        <h5 className= "header_12">پنل ورود به آشنا</h5>
+        {/* <h2>سلام</h2> */}
+        <img src={logo} alt = "logo" width="200" height="200" />
+        
+        
+        <div className="top_login_1">
+        <hr />
+        <h5 href="" className="direct-to-signup">   <a>اگر حساب کاربری ندارید،ثبت‌نام کنید</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href = "http://localhost:3000/signup"> عضویت</a></h5>
+        <hr/>
+        </div>
+        <form onSubmit={e => this.props.handle_login(e, this.state.data)}>
+        <FormGroup className="top_login" >
+        <p className="title_login"> ایمیل</p>
+        <input name = "UserName" value = {this.state.UserName} 
+        onChange={this.handleChange} 
+        placeholder = "ایمیل خود را وارد نمایید"className= "input_login" 
+        type = "email" required></input>
+        </FormGroup>
+
+        <FormGroup >
+        <p className="title_login">رمز عبور</p>
+        <input name = "Password" onChange={this.handleChange} value = {this.state.Password} 
+        placeholder = "رمز عبور خود را وارد نمایید"className= "input_login" 
+        type = "password" required></input>
+        </FormGroup>
+        <Button type="submit" variant="success" size="md" className= "login-button" block>
+        ورود
+        </Button>
+        </form>
+       
+        
+
+        
+      </div>
+    )
+
     
     
       
     return (
+      
+
+      
+        <div>
         
-      <div></div>
+        {LoginForm}
+        </div>
+      
+      
+      
+      
       );
   }
 }
