55d58b8bf5338fae89868eb4b0af91f4eecf86f4
hfa1377 <hfa1377@gmail.com>
2019-07-08 18:26:26 +0430

    Add benefactor profile detailes and edit

---
+++ src/containers/benefactorProfile/benefactorDetails.js
@@ -0,0 +1,108 @@
+import React,{Component} from "react";
+import "./benefactorDetails.css";
+import {Alert,Button} from "react-bootstrap";
+import "../../bootstrap/css/bootstrap.min.css";
+import logo from "../../assets/charity2-01.png";
+class BenefactorDetails extends Component {
+    state = {
+        name : "",
+        image : "",
+        profile : {
+            Name : "",
+            PhoneNum : "",
+            Email : "",
+        }
+    }
+    componentWillMount() {
+        fetch("http://127.0.0.1:8000/my_profile/"+localStorage.getItem('name'), {
+          name : "profname",  
+          headers: {
+                Authorization: `Token ${localStorage.getItem('token')}`
+              }
+        })
+          .then(response => response.json())
+           .then(json => this.saveData(json))
+    }
+    saveData = (json) => {
+        this.setState({name : json.Name});
+        this.setState({image: json.Image});
+        var profile = {...this.state.profile}
+        profile.Name = json.Name;
+        profile.PhoneNum = json.PhoneNumber;
+        profile.Email = json.Email;
+        this.setState({profile});
+
+    }
+    render () {
+        const profile = (
+            <div className = "profile_container">
+                <h4 className="name">{this.state.name}</h4>
+                
+                <Alert variant="dark">
+                {/* <Alert.Heading>Hey, nice to see you</Alert.Heading> */}
+                <p>
+                    {this.state.profile.Name}
+                    <i class="material-icons space">
+                    account_box
+                    </i>
+                </p>
+                
+                <hr />
+                <p>
+                    {this.state.profile.PhoneNum}
+                    <i class="material-icons space">
+                    call
+                    </i>
+                </p>
+                <hr/>
+                <p >
+                {this.state.profile.Email}
+                <i class="material-icons space">
+                email
+                </i>
+                </p>
+                
+                </Alert>
+            </div>
+        )
+        const header = (
+            <div className = "header_profile">
+                <div>
+                <img className = "charity_picture"src = {this.state.image} width = "90" height = " 80"alt= "pic"></img>
+                </div>
+                <div className = "username_header">
+                    <a>{this.state.name}</a>
+                </div>
+                <a href = {"/profile/person/edit/"+this.state.name}><Button className = "createPostButton"  variant="light">ویرایش مشخصات</Button></a>
+            </div>
+        )
+        const pannel = (
+            <div className="container_pannel">
+                <img src={logo} className="pix_charityProfile" alt = "logo" width="150" height="150" />
+                <Button variant="primary" className= "button_pannel" size="lg" block>
+                    خانه
+                </Button>
+                <Button variant="primary" className= "button_pannel" size="lg" block>
+                    دنبال شدگان
+                </Button>
+                <Button href={"/profile/person/"+this.state.name} variant="primary" className= "button_pannel" size="lg" block>
+                پروفایل               
+                </Button>
+
+                
+              
+                
+            </div>
+        )
+        return(
+            <div>
+                {header}
+                {pannel}
+                {profile}
+
+            </div>
+        )
+    }
+}
+
+export default BenefactorDetails;
---
+++ src/containers/benefactorProfile/benefactorEdit.js
@@ -0,0 +1,177 @@
+import React, {Component} from "react";
+import {Button} from "react-bootstrap";
+import logo from "../../assets/charity2-01.png";
+import "./benefactorDetails.css";
+import "./benefactorEdit.css";
+import {Alert} from 'react-bootstrap';
+import {FormGroup} from "react-bootstrap";
+import {InputGroup} from 'react-bootstrap'
+const axios = require("axios");
+class CharityEditProfile extends Component {
+
+    state = {
+        name : "",
+        image : "",
+        PhoneNumber : "",
+        Email : "",
+        file : null
+    }
+    onFormSubmit(event,data){
+        console.log("ready to send data");
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('Name',this.state.name);
+        formData.append('Image',this.state.file);
+        formData.append('PhoneNumber',this.state.PhoneNumber);
+        formData.append('Email',this.state.Email);
+        console.log("ready to send data")
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data',
+                Authorization: `Token ${localStorage.getItem('token')}`
+            }
+        };
+        
+        axios.put('http://127.0.0.1:8000/my_profile/edit/'+localStorage.getItem('name'),formData,config);
+        this.props.history.push(`/profile/person/${this.state.name}`);
+          
+    };
+    componentWillMount() {
+        console.log(localStorage.getItem('name'));
+        
+        fetch("http://127.0.0.1:8000/my_profile/edit/"+localStorage.getItem('name'), {
+          name : "profname",  
+          headers: {
+                Authorization: `Token ${localStorage.getItem('token')}`
+              }
+        })
+          .then(response => response.json())
+           .then(json => this.saveData(json))
+    }
+    saveData = (json) => {
+        this.setState({name:json.Name});
+        this.setState({image:json.Image});
+        this.setState({PhoneNumber:json.PhoneNumber});
+        this.setState({Email:json.Email});
+
+    }
+    handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState(prevState => {
+          const newState = {...prevState};
+          newState[name] = value;
+          return newState;
+        })
+        console.log(this.state)
+    }
+    SetImage = (event) =>{
+        this.setState({file:event.target.files[0]});
+    }
+
+    render() {
+
+        const profile = (
+            <div className = "profile_containerEdit">
+                
+                <Alert variant="info">
+                <Alert.Heading>ویرایش پروفایل</Alert.Heading>
+    
+                
+                <form onSubmit = {event=> this.onFormSubmit(event,this.state)}>
+                <p>
+                <hr />
+                
+                <FormGroup className="top_login" >
+                    <InputGroup></InputGroup>
+                            <p 
+                            className="title_login"><i class="material-icons space">
+                            call
+                            </i></p>
+                            <input 
+                            name = "PhoneNumber"
+                            value = {this.state.PhoneNumber}
+                            onChange = {this.handleChange}
+                            className= "input-editProfile" 
+                            ></input>
+                </FormGroup>
+                <hr />
+                <FormGroup className="top_login" >
+                            <p 
+                            className="title_login"><i class="material-icons space">
+                            email
+                            </i></p>
+                            <input 
+                            name = "Email"
+                            value = {this.state.Email}
+                            onChange = {this.handleChange}
+                            className= "input-editProfile" 
+                            type = "email"></input>
+                </FormGroup>
+                
+                <hr/>
+                <FormGroup className="top_login" >
+                            <p 
+                            className="title_login"><i class="material-icons space">
+                            image
+                            </i></p>
+                            <input 
+                            type="file"
+                            name = "image"
+                            onChange = {this.SetImage}
+                            className= "input-editProfile" 
+                            ></input>
+                </FormGroup>
+                    
+                </p>
+                <hr />
+                <Button type="submit" variant="success">ویرایش مشخصات</Button>
+                </form>
+                </Alert>
+            </div>
+        )
+
+        const header = (
+            <div className = "header_profile">
+                <div>
+                <img className = "charity_picture"src = {this.state.image} width = "90" height = " 80"alt= "pic"></img>
+                </div>
+                <div className = "username_header">
+                    <a>{this.state.name}</a>
+                </div>
+                
+            </div>
+        )
+        const pannel = (
+            <div className="container_pannel">
+                <img src={logo} className="pix_charityProfile" alt = "logo" width="150" height="150" />
+                <Button variant="primary" className= "button_pannel" size="lg" block>
+                    خانه
+                </Button>
+                <Button variant="primary" className= "button_pannel" size="lg" block>
+                    دنبال کنندگان
+                </Button>
+                <Button href={"/profile/person/"+this.state.name} variant="primary" className= "button_pannel" size="lg" block>
+                    پروفایل
+                </Button>
+              
+                
+            </div>
+        )
+
+
+
+
+
+
+        return(
+            <div>
+                {header}
+                {pannel}
+                {profile}
+
+            </div>
+        )
+    }
+}
+export default CharityEditProfile;
