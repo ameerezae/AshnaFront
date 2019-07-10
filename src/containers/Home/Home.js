202c05f9a2dc7832bd1a1ead85edccede65616de
hfa1377 <hfa1377@gmail.com>
2019-07-10 05:58:15 +0430

    Compele home page UI

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,31 +4,164 @@ import { doWhileStatement } from '@babel/types';
  import Logo from '../../assets/charity2-01.png';
  import Seprator from './Seprator';
  import News from './News';
+ import Footer from './Footer';
+import Statistics from './Statistics';
+import {Navbar,Button,Form,NavDropdown,Nav,FormControl,Dropdown} from 'react-bootstrap';
+import Commite from "../../assets/commite.png";
+import Sepas from "../../assets/sepas.png";
+import Otism from "../../assets/otism.png";
+import KarAfarini from "../../assets/karafarini.png";
+import insta_img from '../../assets/insta-logo.png';
+import twitter_img from '../../assets/twitter-logo.png';
+import facebook_img from '../../assets/facebook-logo.png';
 class Introduction extends Component {
+  state = {
+    name : "",
+    charities : [
+      {
+        name : "کمیته امداد امام خمینی",
+        pic : Commite
+      },
+      {
+        name : "انجمن اتیسم ایران",
+        pic : Otism
+      },
+      {
+        name : "موسسه خیریه سپاس",
+        pic : Sepas
+      },
+      {
+        name : "کانون کارافرینی ایران",
+        pic : KarAfarini
+      }
+
+    ]
+  }
   render() {
+  //   const homeNavbar = (
+  //     <div>
+  //         <Navbar bg="light" expand="md" >
+  //             <Navbar.Brand>آشنا</Navbar.Brand>
+  //             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //             <Navbar.Collapse id="basic-navbar-nav ">
+  //                 <Nav className="mr-auto">
+  //                 <Nav.Link className="nav-text-color" href="/login">ورود</Nav.Link>
+  //                 <NavDropdown title="ثبت نام" id="basic-nav-dropdown">
+  //                     <NavDropdown.Item href="/signup/charity">ثبت خیریه</NavDropdown.Item>
+  //                     <NavDropdown.Divider />
+  //                     <NavDropdown.Item href="/signup/person">ثبت نیکوکار</NavDropdown.Item>
+  //                 </NavDropdown>
+  //                 </Nav>
+  //                 <Form inline>
+  //                 <Button href="/charities" variant="outline-success">سازمان ها</Button>
+  //                 </Form>
+  //             </Navbar.Collapse>
+  //         </Navbar>
+  //     </div>
+  // );
+    const homeNavbar = (
+      <div>
+               <Navbar bg="light" expand="md" >
+                   <Navbar.Brand>آشنا</Navbar.Brand>
+                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                   <Navbar.Collapse id="basic-navbar-nav ">
+                       <Nav className="mr-auto">
+                       <Nav.Link className="nav-text-color" >میریزدان سیدی</Nav.Link>
+                       <Nav.Link className = "nav-text-color">خروج</Nav.Link>
+                       </Nav>
+                       <Form inline>
+                       <Button href="/charities" variant="outline-success">سازمان ها</Button>
+                       </Form>
+                   </Navbar.Collapse>
+               </Navbar>
+           </div>
+    );
+
     return (
       <div>
+        {homeNavbar}
       <div className="Intr-box">
-          
-          <p id="intr-text">
+          <div className = "row">
+            <div className = "col-sm-6">
+          <p  id="intr-text">
             <h3 id="intr-title">آشنا</h3>
-            کمبنتشستیب سکمتنب سکنمیب تشکسمت بستب کمستبنمبنتشستیب سکمتنب سکنمیب تشکسمت بستب کمستبنمبنتشستیب سکمتنب سکنمیب تشکسمت بستب کمستبنمبنتشستیب سکمتنب سکنمیب تشکسمت بستب کمستبن
+            
+            در کنار هم تلاش میکنیم ایران را جای بهتری کنیم
+           <br/>
+            انسان خوشبخت نمی شود اگر
+برای خوشبختی دیگران نکوشد.
           </p>
-          <img src={Logo} id="intr-img"></img>
-          
+          </div>
+          <div className = "col-sm-6">
+          <img className = "responsive-images-2 home-picture" src={Logo}></img>
+          </div>
+          </div>
         </div>
-        
-        <Seprator></Seprator>
-        
-        <News></News>
+        {/* <Seprator
+        class = "Seprator"
+        text = "آخرین خیریه‌ها"></Seprator>
 
+        <Seprator
+        class = "Seprator-2"
+        text = "تا این لحظه"></Seprator> */}
+        <div className = "row">
+        <p className = "center-title title-center">آخرین خیریه ها</p>
+        </div>
+        <hr/>
+        <div className="row">
+          {this.state.charities.map(element =>
+          <div className = "col-sm-3">
+          <figure>
+            <img alt="ax" src={element.pic} className="center responsive-images" width="150" height="150"/>
+            <figcaption>
+                <hr/>
+                <div className="search-text">
+                <span className = "home-text-center center">{element.name}</span>
+                </div>
+                <hr/>  
+            </figcaption>
+            </figure>
+          </div>)}
         </div>
 
+        <div className = "row">
+        <p className = "center-title title-center">تا این لحظه</p>
+        </div>
+        <hr/>
+        <div className = "row">
+            <div className = "center">
+                <b>
+                  <div className="row statis-box">
+                    <div className="col-sm-4 ">
+                    <p className="sta-text">331240 مرتبه</p>
+                    <br/>
+                    <p className = "sta-text-2">تعداد حمایت های انجام شده از طریق آشنا</p> 
+                    </div>
+                    <div className="col-sm-4">
+                      <p className="sta-text">4021 عدد</p>
+                      <br/>
+                      <p className = "sta-text-2">تعداد موسسات ثبت شده</p>
+                    </div>
+                    <div className="col-sm-4">
+                      <p className="sta-text">9,365,234,000 تومان</p>
+                      <br/>
+                      <p className = "sta-text-2">حمایت مالی دریافت شده</p> 
+                    </div>
+                </div>
+                </b>
+            </div>
+      </div>
+      <div className="Footer">
+        <img className = "footer-image"src={facebook_img} width="50" height="50" ></img>
         
+        <img className = "footer-image" src={twitter_img } width="50" height="50"></img>
+        <img className = "footer-image" src={insta_img} width="50" height="50"></img>
+      </div>
 
-        
 
-        
+
+
+        </div>
     );
   }
 }
--- src/containers/Home/Seprator.js
+++ src/containers/Home/Seprator.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import  './Seprator.css';
 import { doWhileStatement } from '@babel/types';
 
-class Seprator extends Component {
-  render() {
-    return (
-      <div className="Seprator">
-        <hr id="hr1"></hr>
-        <div  id="septext">
-          آخرین خیریه ها
-        </div>
-        <hr id="hr2"></hr>
 
-      </div>
-    );
-  }
+
+// class Seprator extends Component {
+//   render() {
+//     return (
+//       <div className="Seprator">
+//         <hr id="hr1"></hr>
+//         <div  id="septext">
+//           آخرین خیریه ها
+//         </div>
+//         <hr id="hr2"></hr>
+
+//       </div>
+//     );
+//   }
+// }
+// export default Seprator;
+
+const Seprator = (props) => {
+  return (
+          <div className= {props.class}>
+         <hr id="hr1"></hr>
+         <div  id="septext">
+           {props.text}
+         </div>
+         <hr id="hr2"></hr>
+
+       </div>
+
+  )
 }
 export default Seprator;
-
-
